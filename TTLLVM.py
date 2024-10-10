ef2cf14683f71c60723e9548524ece3b420ba85b
vnklaimis <lvmalatkovskis@gmail.com>
2024-10-10 08:52:04 +0300

    Base code

--- TTLLVM.py
+++ TTLLVM.py
@@ -1,1 +1,22 @@
-#Test
+import viz 
+import vizact
+viz.go() 
+# Importējiet un novietojiet pašu veidotu objektu 
+customObject = viz.add('path/to/your/object.osgb') 
+customObject.setPosition([0, 0, 5]) 
+
+customObject.setPosition([0, 0, 5]) 
+anotherObject.setPosition([2, 0, 5]) 
+
+def onPick(e): 
+    if e.object.valid(): 
+        e.object.color(viz.RED) 
+        move = vizact.moveTo([0, 2, 0], time=1) 
+        e.object.addAction(move) 
+        viz.callback(viz.PICK_EVENT, onPick) 
+        viz.mouse(viz.OFF) 
+vizact.onkeydown('p', viz.mouse.pick) 
+
+def updateCamera(): 
+    viz.MainView.move([0, 0, 0.1]) 
+vizact.ontimer(0, updateCamera)
